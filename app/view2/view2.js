0f9e1eeee1e88edda7309b9da689ad25885913fe
Wu, Leo <Guodong.X.Wu@bnymellon.com>
2018-06-13 15:41:59 +0100

    map add map for view 2

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -1,14 +1,28 @@
 'use strict';
 
-angular.module('myApp.view2', ['ngRoute'])
+angular.module('myApp.view2', ['ngRoute', 'ngMap'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view2', {
-    templateUrl: 'view2/view2.html',
-    controller: 'View2Ctrl'
-  });
-}])
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/view2', {
+      templateUrl: 'view2/view2.html',
+      controller: 'View2Ctrl'
+    });
+  }])
 
-.controller('View2Ctrl', [function() {
+  .controller('View2Ctrl', ['$scope', 'ngMap', function ($scope, NgMap) {
 
-}]);
+    $scope.test = function () {
+      // send the coordinate to api
+      console.log($scope.markers[0].getPosition());
+    }
+
+    $scope.markers = [
+      { address: "[40.71, -74.21]" },
+      { address: "[40.72, -74.20]" },
+    ];
+    NgMap.getMap().then(function (map) {
+      console.log(map.getCenter());
+      console.log('markers', map.markers);
+      console.log('shapes', map.shapes);
+    });
+  }]);
